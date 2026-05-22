f82d00feb65d662544b9862b9fdc29f27037f3e8
PabloVecilla <pablovecilla8@gmail.com>
2026-05-22 17:20:00 +0200

    PV_add rate limiter to login

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { login, getMe, logout } from "../controllers/auth.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
-
+import { loginLimiter } from "../middlewares/rateLimit.middleware.js";
 const router = express.Router();
 
+
 // POST http://localhost:3000/api/auth/login
-router.post("/login", login);
+router.post("/login", loginLimiter, login);
 // GET http://localhost:3000/api/auth/me
 router.get("/me", authMiddleware, getMe);
 // POST http://localhost:3000/api/auth/logout
